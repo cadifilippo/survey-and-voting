bc6d04d66d21430f4cb0e1e62a5363b17cffc22c
Carlos Difilippo <cadifilippo@gmail.com>
2022-02-28 20:56:35 -0300

    fix: add test config

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@ import '@testing-library/jest-dom';
 jest.mock('@supabase/supabase-js', () => ({
   createClient: jest.fn()
 }));
+
+jest.mock('./context/auth', () => ({
+  useAuth: () => ({
+    login: jest.fn(),
+    reload: jest.fn()
+  })
+}));
