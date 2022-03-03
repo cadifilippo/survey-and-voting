7242a01607fa5938de49d8617ae2069e1cf98696
Carlos Difilippo <cadifilippo@gmail.com>
2022-03-02 23:03:22 -0300

    style: change login styles and add login test

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
 export { ReactComponent as GithubIcon } from './svg/github.svg';
 export { ReactComponent as LogoutIcon } from './svg/logout.svg';
+import LoginBg from './img/login-bg.jpg';
+export const BgLogin = LoginBg;
--- src/features/login/views/Login.js
+++ src/features/login/views/Login.js
@@ -1,5 +1,5 @@
 import styles from './Login.module.css';
-import { GithubIcon } from '../../../assets';
+import { BgLogin, GithubIcon } from '../../../assets';
 import { useAuth } from '../../../context/auth';
 import { useEffect } from 'react';
 
@@ -11,10 +11,15 @@ const Login = () => {
   }, []);
 
   return (
-    <section className={styles.login}>
+    <section className={styles.login} style={{ backgroundImage: `url(${BgLogin})` }}>
       <div className={styles.form}>
         <h1 className={styles.title}>Survey & Voting</h1>
-        <button className={styles.button} onClick={login}>
+        <button
+          data-testid="github"
+          className={styles.button}
+          style={{ backgroundImage: `url(${BgLogin})` }}
+          onClick={login}
+        >
           <GithubIcon width={40} height={40} />
           Sign In with Github
         </button>
